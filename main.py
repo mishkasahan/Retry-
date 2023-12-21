8082043ae972239f8962e050b2b8b0bbd74d8012
Misha Sahan <mishkasahan@gmail.com>
2023-12-21 17:29:22 +0200

    Memoize

--- main.py
+++ main.py
@@ -1,22 +1,30 @@
-def retry(num=1):
-    def decor(func):
-        def inner(*args, **kwargs):
-            print("Start")
-            for i in range(num):
-                try:
-                    func()
-                    break
-                except Exception as e:
-                    print(e)
-                print("Finish")
-        return inner
-    return decor
+def memoize(func):
+    kesh = {}
+
+    def inner(n):
+        if n not in kesh.keys():
+            print("Результат виконання функції")
+            kesh[n] = func(n)
+            return kesh[n]
+        else:
+            print("Результат взято з кешу")
+            print(kesh[n])
+            return kesh[n]
+    return inner
 
 
-@retry(10)
-def fd():
-    for i in range(10):
-        print(i/0)
+@memoize
+def faktorial(n):
+    rez = 1
+    for i in range(1, n):
+        rez *= i
+    print(rez)
+    return rez
 
 
-fd()
+faktorial(5)
+print(100 * '*')
+faktorial(5)
+print(100 * '*')
+faktorial(4)
+print(100 * '*')
